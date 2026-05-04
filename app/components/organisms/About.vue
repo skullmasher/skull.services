a2f64d5644ef4b642c2f299551fa77d7cb676fce
Skullmasher Heartless <4488386+SkullMasher@users.noreply.github.com>
2026-05-04 15:32:14 +0200

    feat: change order of business card

--- app/components/organisms/About.vue
+++ app/components/organisms/About.vue
@@ -1,6 +1,6 @@
 <template>
-  <section id="about" class="px-2 flex flex-col md:flex-row justify-center items-center">
-    <CoolPictures />
+  <section id="about" class="px-2 pt-8 flex flex-col md:flex-row-reverse justify-center items-center">
     <BusinessCard />
+    <CoolPictures />
   </section>
 </template>
